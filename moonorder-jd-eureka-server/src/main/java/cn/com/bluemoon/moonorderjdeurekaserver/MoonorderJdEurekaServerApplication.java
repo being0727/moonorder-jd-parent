33192d3af127c16b8c563873662c6deb97b2f632
xuzhansheng <xuzhansheng@bluemoon.com.cn>
2018-12-29 15:30:59 +0800

    测试git文件

--- moonorder-jd-eureka-server/src/main/java/cn/com/bluemoon/moonorderjdeurekaserver/MoonorderJdEurekaServerApplication.java
+++ moonorder-jd-eureka-server/src/main/java/cn/com/bluemoon/moonorderjdeurekaserver/MoonorderJdEurekaServerApplication.java
@@ -1,5 +1,7 @@
 package cn.com.bluemoon.moonorderjdeurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,8 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class MoonorderJdEurekaServerApplication {
-
+	private Logger logger= LoggerFactory.getLogger(MoonorderJdEurekaServerApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(MoonorderJdEurekaServerApplication.class, args);
 	}
 
