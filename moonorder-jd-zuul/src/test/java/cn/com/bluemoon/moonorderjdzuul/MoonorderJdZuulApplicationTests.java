6896e422edd4e96fe43c3188fb0e529527923001
xuzhansheng <xuzhansheng@bluemoon.com.cn>
2019-04-03 11:41:47 +0800

    修改zuul配置文件,防止查询时间过长时导致连接超时

--- moonorder-jd-zuul/src/test/java/cn/com/bluemoon/moonorderjdzuul/MoonorderJdZuulApplicationTests.java
+++ moonorder-jd-zuul/src/test/java/cn/com/bluemoon/moonorderjdzuul/MoonorderJdZuulApplicationTests.java
@@ -5,12 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MoonorderJdZuulApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws  Exception {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		// 2019-03-25 09:00:00
+		Date data = simpleDateFormat.parse("2019-03-25 09:00:00");
+		long time=data.getTime();
+		System.err.println(time+"");
 	}
 
 }
